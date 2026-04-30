126173204cc9c9f429e30853182785f0703ed688
chesedo <pieter@chesedo.me>
2026-04-30 15:51:20 +0200

    Update to rl_big2_8.6 branch

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
     # - For specific commit: "github:redis/redis/abcdef123456789"
     # - For local source: use inputs.redis.url = "/path/to/local/redis";
     redis = {
-      url = "git+ssh://git@github.com/redislabsdev/Redis.git?ref=rse_big2_8.6";
+      url = "git+ssh://git@github.com/redislabsdev/Redis.git?ref=rl_big2_8.6";
       flake = false;
     };
 
@@ -59,7 +59,7 @@
     # 2. This approach gives us more control over the speedb version
     # 3. We can copy speedb to a writable location during the build
     speedb = {
-      url = "git+ssh://git@github.com/redislabsdev/speedb-ent?rev=d52df54a12b45116e42163fc0f4e95ccf535ee41";
+      url = "git+ssh://git@github.com/redislabsdev/speedb-ent?rev=3cc94f283eedccfb203d3c48af65a057dca15799";
       flake = false;
     };
   };
